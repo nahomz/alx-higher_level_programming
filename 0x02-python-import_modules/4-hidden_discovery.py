28567a74b01b8352439ba39460feab78f09c2961
YesseJr <thonnieyesse@hotmail.com>
2021-11-18 17:05:46 +0300

    The discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
     for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+        if not i.startswith("__"):
+            print("{:s}".format(i)
