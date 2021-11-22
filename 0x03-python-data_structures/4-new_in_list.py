152a383c324d56f31a0206356ef1302eb164c011
nahomz <nahomzelalem11@gmail.com>
2021-11-22 18:41:21 +0000

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,13 +2,12 @@
 
 
 def new_in_list(my_list, idx, element):
-    """Makes copy of a list, replce element at certain index in copy
-       while leaving originial unmodified.
+    """
+    A function that replaces an element in a list at 
+    a specific position without modifying the original list
     """
     if idx < 0 or idx > len(my_list) - 1:
-        return my_list[:]
-    else:
-        new_list = my_list[:]
-        new_list[idx] = element
-        return new_list
-    
+        return my_list
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of ints"""
-    for row in matrix:
-        for i in row:
-            if i != row[-1]:
-                print("{:d}".format(i), end="")
-            else:
-                print("{:d}".format(i), end="")
-        print()
+    """
+    A function that prints a matrix of integers.
+    """
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
