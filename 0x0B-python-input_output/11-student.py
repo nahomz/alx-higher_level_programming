459d2396502ae26db37415d4d26417456fb4dd55
nahomz <nahomzelalem11@gmail.com>
2021-12-21 20:53:57 +0000

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,48 +1,23 @@
-#!/usr/bin/pyton3
-"""Defiantion for a student class
-"""
+#!/usr/bin/python3
+'''Module for Student class.'''
 
 
 class Student:
-   """a class for a student
-   """
+    '''Class for jsonification.'''
+    def __init__(self, first_name, last_name, age):
+        '''Constructor.'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-   def __init__(self, first_name, last_name, age):
-       """Initalizes a student object
-       """
+    def to_json(self, attrs=None):
+        '''Retrieves dictionary with filter.'''
+        if type(attrs) is list and all([type(x) == str for x in attrs]):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__.copy()
 
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
-
-   def to_json(self, attrs=None):
-       """returns a json representatiion of
-       the object
-       """
-
-       if attrs != None and not isinstance(attrs, list):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs != None and any(not isinstance(attr, str) for attr in attrs):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs == None:
-           return self.__dict__
-
-       json_dic = {}
-       for key in attrs:
-           if key in self.__dict__.keys():
-               json_dic[key] = self.__dict__[key]
-
-       return json_dic
-
-    def relod_from_json(self, json):
-    """replaces object's attributes
-    """
-
-        for k, v in json.items():
-            setattr(self, k, v)
-
-
-
-
+    def reload_from_json(self, json):
+        '''Loads attributes from json.'''
+        for key, value in json.items():
+            self.__dict__[key] = value
