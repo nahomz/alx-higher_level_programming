ea5d44867b176a52517efd0f482a25de7ce0343f
nahomz <nahomzelalem11@gmail.com>
2022-02-02 05:31:58 +0000

    fix

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,2 @@
--- lists all records of 'second_table' of database hbtn_0c_0
--- in descending order on the 'score' column
-SELECT score, NAME
-FROM second_table
-ORDER BY score DESC;
+-- Prints the sorted database
+SELECT score, name FROM second_table ORDER BY score DESC, name;
--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,20 +1,9 @@
--- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- in MySQL Server
-ALTER DATABASE
-	hbtn_0c_0
-	CHARACTER SET = utf8mb4
-	COLLATE = utf8mb4_unicode_ci;
-
--- convert table "first_table' to UTF8
-ALTER TABLE
-	hbtn_0c_0.first_table
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
-
--- convert column 'name' to UTF8
-ALTER TABLE
-	hbtn_0c_0.first_table
-	MODIFY name
-	VARCHAR(256)
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+-- Change database to utf-8
+-- Use hbtn_0c_0 database
+USE hbtn_0c_0;
+-- Change name to utf-8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+-- Change table to utf-8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change database to utf-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
