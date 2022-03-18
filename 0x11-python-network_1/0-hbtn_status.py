9d13282e46d3354fccba550ed1fd651b32a1d0e0
nahomz <nahomzelalem11@gmail.com>
2022-03-18 20:21:14 +0000

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-''' fetches https://intranet.hbtn.io/status '''
-from urllib.request import urlopen
+""" Script that fetches from a url using urllib package"""
 
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    with urlopen(url) as result:
-        output = result.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(output)))
-        print('\t- content: {}'.format(output))
-        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
+import urllib.request as request
+
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
